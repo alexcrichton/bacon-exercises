f183505860ec7c2f5e15eab6c8d679d7d04f5d77
Alex Crichton <alex@alexcrichton.com>
2023-09-01 12:37:18 -0700

    Something is working

---
+++ src/shims.js
@@ -0,0 +1,53 @@
+export function fileURLToPath(args) {
+  throw new Error('unimplemented fileURLToPath')
+}
+
+export function spawn() {
+  throw new Error('unimplemented spawn')
+}
+
+export function readFile() {
+  throw new Error('unimplemented readFile')
+}
+
+export function resolve() {
+  throw new Error('unimplemented resolve')
+}
+
+export function unlink() {
+  throw new Error('unimplemented unlink')
+}
+
+export function dirname() {
+  throw new Error('unimplemented dirname')
+}
+
+export function basename() {
+  throw new Error('unimplemented basename')
+}
+
+export function extname() {
+  throw new Error('unimplemented extname')
+}
+
+export function minify() {
+  throw new Error('unimplemented minify')
+}
+
+export function writeFile() {
+  throw new Error('unimplemented writeFile')
+}
+
+export const argv0 = 'argv0';
+
+export function tmpdir() {
+  throw new Error('unimplemented tmpdir')
+}
+
+export function mkdir() {
+  throw new Error('unimplemented mkdir')
+}
+
+export default function() {
+  throw new Error('unimplemented default')
+}
