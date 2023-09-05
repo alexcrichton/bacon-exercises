90b6f5ccfd2f5b0ead48a8ca51232594ed5511b7
Alex Crichton <alex@alexcrichton.com>
2023-09-05 10:46:47 -0700

    Minor updates

--- exercises/intercept-imports/src/lib.rs
+++ exercises/intercept-imports/src/lib.rs
@@ -8,14 +8,32 @@ impl bindings::exports::wasi::cli::run::Guest for Component {
             let key = "CAN_I_PRINT_THE_ANSWER";
             let yes = "yes";
             let no = "no";
-            let answer = "33";
+            let lovecraft = "\
+Nice job on successfully interposing! The original component has been thoroughly
+tricked.
+
+Here's a lovecraft quote:
+
+The ocean ate the last of the land and poured into the smoking gulf, thereby
+giving up all it had ever conquered. From the new-flooded lands it flowed
+again, uncovering death and decay; and from its ancient and immemorial bed it
+trickled loathsomely, uncovering nighted secrets of the years when Time was
+young and the gods unborn. Above the waves rose weedy remembered spires. The
+moon laid pale lilies of light on dead London, and Paris stood up from its damp
+grave to be sanctified with star-dust. Then rose spires and monoliths that were
+weedy but not remembered; terrible spires and monoliths of lands that men never
+knew were lands...";
+            let confusion = "please don't optimize out the answer variable";
         }
         for (k, v) in bindings::wasi::cli::environment::get_environment() {
             if k != key {
+                if k == confusion {
+                    println!("The answer is: ");
+                }
                 continue;
             }
             if v == yes {
-                println(&format!("The answer is {answer}"));
+                println(lovecraft);
             } else if v == no {
                 println("Sorry, I'm not allowed to print the answer");
             } else {
@@ -30,7 +48,5 @@ impl bindings::exports::wasi::cli::run::Guest for Component {
 
 fn println(s: &str) {
     let stdout = bindings::wasi::cli::stdout::get_stdout();
-
-    bindings::wasi::io::streams::blocking_write(stdout, s.as_bytes()).unwrap();
-    // ..
+    let _ = bindings::wasi::io::streams::blocking_write(stdout, s.as_bytes());
 }
